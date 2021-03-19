deb22af8a781b2d780de9f89c58bf09cf81ad9f1
Michael Kerry <mkerry@fas.harvard.edu>
2021-03-19 16:27:00 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pydb",
-    version="0.0.1",
+    version="0.0.2",
     author="Michael Kerry",
     author_email="michael_kerry@harvard.edu",
     description="A package to facilitate connecting to an oracle DB from a python application",
